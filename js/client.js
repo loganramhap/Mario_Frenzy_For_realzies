239b996897c2d1112817ea2634109d00315af3ba
Logan Ramhap <lramhap@gmail.com>
2019-05-06 14:37:48 -0400

    Yo, IDK what the fuck is going on here

--- js/client.js
+++ js/client.js
@@ -1,5 +1,5 @@
-import game.js
 var Client = {};
+//var Logan = new game.qwertyuiop(10, 10, 10)
 Client.socket = io.connect();
 
 Client.sendTest = function(){
@@ -16,13 +16,12 @@ Client.sendClick = function(x,y){
 };
 
 Client.socket.on('newplayer',function(data){
-    Game.qwertyuiop(data.id,data.x,data.y);
+    Game.addNewPlayer(data.id,data.x,data.y);
 });
 
 Client.socket.on('allplayers',function(data){
     for(var i = 0; i < data.length; i++){
-        //Game.addNewPlayer(data[i].id, data[i].x, data[i].y);
-        Game.qwertyuiop(data[i].id, data[i].x, data[i].y);
+        Game.addNewPlayer(data[i].id, data[i].x, data[i].y);
     }
 
     Client.socket.on('move',function(data){
